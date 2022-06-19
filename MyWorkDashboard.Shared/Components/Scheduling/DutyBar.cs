e6d25676d26da385fdbff63c3140f7bf0179975f
Hanyu <8245snake@gmail.com>
2022-06-19 11:07:20 +0900

    タスクの時刻が重なっている場合にバーの幅を分割する機能 #31

--- MyWorkDashboard.Shared/Components/Scheduling/DutyBar.cs
+++ MyWorkDashboard.Shared/Components/Scheduling/DutyBar.cs
@@ -1,14 +1,41 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using MyWorkDashboard.Shared.Duties;
+using MyWorkDashboard.Shared.Services;
 
 namespace MyWorkDashboard.Shared;
 
 public class DutyBar
 {
+    /// <summary>
+    /// モデル
+    /// </summary>
     public Duty Duty { get; }
+    /// <summary>
+    /// Top位置
+    /// </summary>
     public int Top { get; }
+    /// <summary>
+    /// 高さ
+    /// </summary>
     public int Height { get; }
+
+    /// <summary>
+    /// 選択中か否か
+    /// </summary>
     public bool IsSelected { get; set; }
+
+    /// <summary>
+    /// 多重度
+    /// </summary>
+    public int Multiplicity { get; set; } = 1;
+
+    /// <summary>
+    /// インデックス
+    /// </summary>
+    public int Index { get; set; } = 0;
+
+
     private string _colorCode;
 
     public static int TimeWidth = 40;
@@ -56,7 +83,14 @@ public class DutyBar
         {
             if (_isAbsolutePositioning)
             {
-                return $"top:{Top}px; height:{Height}px; left:{TimeWidth + 5}px; background-color: {_colorCode};";
+                string widthStyle = $"calc(calc(95% - {TimeWidth}px) / {Multiplicity})";
+                string styel = "";
+                styel += $"top:{Top}px;";
+                styel += $"width:{widthStyle};";
+                styel += $"left:calc({TimeWidth}px + calc({Index} * {widthStyle}));";
+                styel += $"height:{Height}px;";
+                styel += $"background-color: {_colorCode};";
+                return styel;
             }
 
             return $"height:{Height}px; background-color: {_colorCode};";
@@ -70,4 +104,22 @@ public class DutyBar
         IsSelected = true;
         Selected?.Invoke(this, EventArgs.Empty);
     };
+
+
+    public bool IsOverlapping(DutyBar other)
+    {
+        if (this.Duty.StartTime >= other.Duty.StartTime && this.Duty.StartTime < other.Duty.EndTime)
+        {
+            // 自身の開始より前に始まって、自身の開始より後に終わっているタスク
+            return true;
+        }
+
+        if (this.Duty.StartTime <= other.Duty.StartTime && this.Duty.EndTime > other.Duty.StartTime)
+        {
+            // 自身の開始より後かつ自身の終了より前に始まっているタスク
+            return true;
+        }
+
+        return false;
+    }
 }
--- MyWorkDashboard.Shared/Components/Scheduling/DutyBarCollection.cs
+++ MyWorkDashboard.Shared/Components/Scheduling/DutyBarCollection.cs
@@ -42,4 +42,20 @@ public class DutyBarCollection
             ItemSelected?.Invoke(this, new DutySelectedEventArgs(dutyBar.Duty));
         }
     }
+
+    /// <summary>
+    /// 重なりを判定して多重度とインデックスを設定する
+    /// </summary>
+    public void CalculateMultiplicity()
+    {
+        foreach (DutyBar bar in Bars)
+        {
+            // １つずつ重なりを見ていく
+            var overlapping = Bars.Where(b => b.IsOverlapping(bar)).OrderBy(b => b.Duty.StartTime).ToArray();
+            bar.Multiplicity = overlapping.Length;
+            bar.Index = Array.IndexOf(overlapping, bar);
+        }
+
+        // todo A-B B-C のように重なっている場合に多重度を3にしてインデックスを振る実装
+    }
 }
